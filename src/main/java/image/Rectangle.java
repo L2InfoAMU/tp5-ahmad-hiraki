fd2374d614c7c33cb8d096928ec50ca1c94d24fe
george <george.bardaghji@etu.univ-amu.fr>
2018-12-06 14:50:47 +0100

    creation of class rectangle

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,27 @@
+package image;
+import javafx.scene.paint.Color;
+import java.util.HashMap;
+
+public class Rectangle implements Shape {
+
+    private Color color;
+    private int width;
+    private int height;
+    private Point leftHigh;
+
+    public Rectangle(int x, int y, int width, int height, Color color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.leftHigh = new Point(x, y);
+    }
+
+    public boolean contains(Point point){
+        boolean width = (leftHigh. x <= point.x && point.x <= leftHigh.x + this.width);
+        boolean height = (leftHigh.y <= point.y && point.y <= leftHigh.y + this.height);
+        return (width && height);
+    }
+
+    public Color getColor(){return this.color; }
+
+}
---
+++ src/main/java/image/VectorImage.java
@@ -0,0 +1,9 @@
+package image;
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class VectorImage implements Image {
+
+
